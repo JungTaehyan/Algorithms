6646d7496b395ce570656098877108a3d2be9bcd
jth9339 <erzangthehua@gmail.com>
2022-06-10 16:31:14 +0900

    11651

--- sorting/11651.py
+++ sorting/11651.py
@@ -4,9 +4,8 @@ num = 0
 list_1 = []
 while num < N:
     x, y = map(int, stdin.readline().split())
-    list_1.append([x, y])
+    list_1.append([y, x])
     num+=1
 list_1.sort()
-
 for k in list_1:
-    print(k[0], k[1])
+    print(k[1], k[0])
