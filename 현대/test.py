e5fb3a92d407a4e6e672734d4ca0b2a1a442c7b2
Teddy <taehyan.jung@cmes.kr>
2024-01-29 10:46:34 +0900

    DFS 예시

--- 현대/test.py
+++ 현대/test.py
@@ -9,12 +9,12 @@ for i in range(9):
     #print(graph[i])
 def dfs(graph, v, visited):
     visited[v] = True
-    print(v, end = ' ')
+    print("v : "+str(v), end = '\n')
     print("visited[v] :  "+str(visited[v]))
     for i in graph[v]:
         print("i :  "+str(i))
         if not visited[i]:
-            print("visited[i] :  "+str(visited[i]))
+            print("visited[i] :  "+str(visited[i])+", i :  "+str(i))
             dfs(graph, i, visited)
 
 dfs(graph, 1, visited)
