ce38003d9d963a05b46c6a75eb9810d23ed7d0b7
Teddy <taehyan.jung@cmes.com>
2024-01-29 23:35:02 +0900

    BFS

--- 현대/DFS_BFS/BFS.py
+++ 현대/DFS_BFS/BFS.py
@@ -22,11 +22,14 @@ def bfs(x,y):
             nx = x+dx[i]
             ny = y+dy[i]
 
-            if nx < 0 or nx >= n or ny < 0 or ny > m:
+            if nx < 0 or nx >= n or ny < 0 or ny >= m:
                 continue
             if graph[nx][ny] == 0:
                 continue
             if graph[nx][ny] == 1:
-                graph[nx][ny] = graph[nx][ny]+1
+                graph[nx][ny] = graph[x][y]+1
                 queue.append((nx,ny))
     return graph[n-1][m-1]
+
+print(bfs(0,0), end = "\n")
+print(graph)
